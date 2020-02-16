65bb972686f50c992f78928cb9266bbfd4683853
Rsam92 <samradji@yahoo.fr>
2020-02-16 20:07:51 +0100

    End part 6: Security

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class UserFixtures extends Fixture
+{
+
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderinterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $user = new User();
+        $user->setUsername('Samia');
+        $user->setPassword($this->encoder->encodePassword($user, 'demo'));
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
